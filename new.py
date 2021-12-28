ca3b3c6729bada06502acda1cbe470972778510c
presty1212 <96751106+presty1212@users.noreply.github.com>
2021-12-27 19:09:58 -0500

    Update new.py

--- new.py
+++ new.py
@@ -1,9 +1,9 @@
 key='8792623f-6239-4b8b-8e73-75e1fdd5f58b'
-app_name="accgen123233"
-deviceid="17A7F633BD2668F5C58AC8FF2E0DDB52FE738274220CF20A44BD5C421B2F47D483B642ED67AE381E"
+app_name='accgen123233'
+deviceid='17A7F6333BD2668F5C58AC8FF2E0DD852FE738274220CF20A44FBD5C421B24F47D483B642ED67AE381E'
 emaill="e8bf0gjt9@xojxe.com"
-passwordd="tempemail"
+passwordd="tempmail"
 custompwd="fakeaccpwd"
-private="5fb3cda0-f95b-03b3-33c9-f85c37ad1577"
-chatlink="https://aminoapps.com/c/Prestywesty"
+url="https://replit.com/join/drmimsnjja-prestonberns'
+chatlink="http://aminoapps.com/p/x8ntuj"
 nickname="itsme"
